31c5328e4716aed559acc5dcc6606e3a803263cf
jaiminpatel1999 <jdp9662@gmail.com>
2024-02-11 15:43:44 -0500

    Initial Commit

---
+++ story_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  story_app
+//
+//  Created by user237118 on 2/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
